fa041606fe1a30665385b7fc8b58eab45771f00d
robcu <robculclasure@Robs-MacBook-Pro.local>
2017-03-22 17:19:09 -0500

    Add class Player

---
+++ src/Player.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private String team;
+    private ArrayList<Card> hand;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+}
