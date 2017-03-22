cc4d8a520c958d2be30a7f21eba1cc81dee64f70
robcu <robculclasure@Robs-MacBook-Pro.local>
2017-03-22 16:14:39 -0500

    Add Card class

---
+++ src/Card.java
@@ -0,0 +1,38 @@
+
+public class Card {
+    String name;
+    String suit;
+    String rank;
+
+    public Card(String name, String suit, String rank) {
+        this.name = name;
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Card() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+}
