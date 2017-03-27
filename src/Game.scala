d2e4cc1455a9581054ad58e95c37556aba972624
tnraptor14 <tnraptor14@hotmail.com>
2017-03-27 16:55:52 -0500

    started Game class

--- src/Game.scala
+++ src/Game.scala
@@ -1,18 +1,31 @@
+import scala.collection.mutable
 
 class Game {
-    /*
-    Evaluate the 4 cards in the play and determine winner
-     */
+    val player1 = new Player()
+    val player2 = new Player
+    val player3 = new Player
+    val player4 = new Player
 
-    /*
-    Determine whether card a person played is valid
-     */
+    val bids = mutable.HashMap("player1" -> 0,
+                                "player2" -> 0,
+                                "player3" -> 0,
+                                "player4" -> 0)
 
-    /*
-    Deal 12 cards to each player and put remaining 5 in cat
-     */
+    var cat: mutable.MutableList[Card] = new mutable.MutableList[Card]
+    var currentBidder: Player = _
+    var winningBidder: Player = _
+    var winningBid: Int = _
 
-    /*
-    Take bids
-     */
+    var leader: Player = _
+    var trump: String = _
+    var currentRound: Int = 1
+
+    def deal(): Unit = {
+        Deck.create()
+        player1.takeCards(Deck.dealMany(12))
+        player2.takeCards(Deck.dealMany(12))
+        player3.takeCards(Deck.dealMany(12))
+        player4.takeCards(Deck.dealMany(12))
+        Deck.dealMany(5).forEach(card => cat += card)
+    }
 }
