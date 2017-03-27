0b1f36b6cb35483a0152ce30f91cc890c4be4e8c
tnraptor14 <tnraptor14@hotmail.com>
2017-03-27 15:02:33 -0500

    update

---
+++ src/Game.scala
@@ -0,0 +1,18 @@
+
+class Game {
+    /*
+    Evaluate the 4 cards in the play and determine winner
+     */
+
+    /*
+    Determine whether card a person played is valid
+     */
+
+    /*
+    Deal 12 cards to each player and put remaining 5 in cat
+     */
+
+    /*
+    Take bids
+     */
+}
